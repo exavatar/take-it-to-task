f7de6e54cee2fa81da8405619d529b7934b8fac7
exavatar <exavatar@gmail.com>
2014-04-28 22:13:05 -0600

    Changes for tonight.

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,12 +1,70 @@
-angular.module('starter.controllers', [])
+angular.module('takeItToTask.controllers', [])
 
-.controller('MenuController', function($scope) {
-  $scope.m = {};
+.controller('MenuController', ['$scope', '$firebase', function($scope, $firebase) {
+  var tasksRef = new Firebase('https://exavatar.firebaseio.com/tasks');
+
+  $scope.m  = {};
+  $scope.fn = {};
+
+  $scope.m.authuser = {email: 'exavatar@gmail.com', password:'B0bbl3'};
+
+  $scope.m.tasks = $firebase(tasksRef);
+
+  $scope.m.auth = new FirebaseSimpleLogin(tasksRef, function(error, user)
+  {
+    if (error) {
+      console.log(error);
+    } else if (user) {
+      console.log(user);
+      console.log($scope);
+      $scope.m.user = user;
+    }
+    else {
+      console.log('user not logged in.');
+    }
+
+  })
 	$scope.m.categories = [
 		{ id: 1, name: 'Personal'},
 		{ id: 2, name: 'Work'}
 	];
-})
+
+  //TOMTODO - in the wrong controller!
+  $scope.fn.addTask = function()
+  {
+    console.log('go?', $scope.m);
+    if($scope.m.taskToAdd)
+    {
+      $scope.m.tasks[$scope.m.user.id].$add($scope.m.taskToAdd);
+      console.log('task added');
+    }
+    //$scope.m.taskToAdd = null;
+  }
+
+  $scope.fn.signIn = function()
+  {
+    $scope.m.auth.login('password', {
+      email: $scope.m.authuser.email,
+      password: $scope.m.authuser.password
+    });
+  }
+
+  $scope.fn.signUp = function()
+  {
+    $scope.m.auth.createUser($scope.m.authuser.email, $scope.m.authuser.password, function(error, user)
+    {
+      if( error )
+      {
+        console.log(error);
+      }
+      else
+      {
+        console.log(user);
+      }
+    })
+  }
+
+}])
 
 .controller('CategoryController', function($scope, $stateParams) {
   //console.log($stateParams);
