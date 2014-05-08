14c2baf586c32ed558d148610d25c22559c7980d
exavatar <exavatar@gmail.com>
2014-05-07 21:24:11 -0600

    Starting to get firebase wired up.  Messing around with how to structure data.  I am thinking about having a tree with users at the root, and each user has a tree of information associated with them.

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,73 +1,21 @@
 angular.module('takeItToTask.controllers', [])
 
-.controller('MenuController', ['$scope', '$firebase', function($scope, $firebase) {
-  var tasksRef = new Firebase('https://exavatar.firebaseio.com/tasks');
+.controller('MenuController', ['$scope', function($scope) {
+  
 
   $scope.m  = {};
   $scope.fn = {};
 
-  $scope.m.authuser = {email: 'exavatar@gmail.com', password:'B0bbl3'};
-
-  $scope.m.tasks = $firebase(tasksRef);
-
-  $scope.m.auth = new FirebaseSimpleLogin(tasksRef, function(error, user)
-  {
-    if (error) {
-      console.log(error);
-    } else if (user) {
-      console.log(user);
-      console.log($scope);
-      $scope.m.user = user;
-    }
-    else {
-      console.log('user not logged in.');
-    }
-
-  })
 	$scope.m.categories = [
 		{ id: 1, name: 'Personal'},
 		{ id: 2, name: 'Work'}
 	];
 
-  //TOMTODO - in the wrong controller!
-  $scope.fn.addTask = function()
-  {
-    console.log('go?', $scope.m);
-    if($scope.m.taskToAdd)
-    {
-      $scope.m.tasks[$scope.m.user.id].$add($scope.m.taskToAdd);
-      console.log('task added');
-    }
-    //$scope.m.taskToAdd = null;
-  }
-
-  $scope.fn.signIn = function()
-  {
-    $scope.m.auth.login('password', {
-      email: $scope.m.authuser.email,
-      password: $scope.m.authuser.password
-    });
-  }
-
-  $scope.fn.signUp = function()
-  {
-    $scope.m.auth.createUser($scope.m.authuser.email, $scope.m.authuser.password, function(error, user)
-    {
-      if( error )
-      {
-        console.log(error);
-      }
-      else
-      {
-        console.log(user);
-      }
-    })
-  }
-
 }])
 
 .controller('CategoryController', function($scope, $stateParams) {
   //console.log($stateParams);
+  console.log("Da scope", $scope);
   $scope.m = {};
   $scope.m.activeCategoryId = $stateParams.categoryId;
   $scope.m.categoryTasks = { 
@@ -90,3 +38,77 @@ angular.module('takeItToTask.controllers', [])
     priority: 'OMG do it now!'
   }
 })
+
+.controller('GlobalController', ['$scope', '$firebase', function($scope, $firebase) {
+  $scope.global = {};
+  $scope.globalFn = {};
+  $scope.global.firebaseRef = new Firebase('https://exavatar.firebaseio.com/tasks');
+
+  //Now people can type it in. --tconnell 2014-05-05
+  //$scope.global.authuser = {email: 'exavatar@gmail.com', password:'B0bbl3'};
+
+  $scope.global.auth = new FirebaseSimpleLogin($scope.global.firebaseRef, function(error, user)
+    {
+    if (error) {
+      console.log(error);
+    } else if (user) {
+      console.log("user", user);
+      console.log("Auth state", $scope.global.auth);
+      $scope.global.user = user;
+      $scope.global.userRef = new Firebase('https://exavatar.firebaseio.com/users/'+ user.id);
+      $scope.global.tasksRef = new Firebase('https://exavatar.firebaseio.com/users/'+ user.id +'/tasks');
+      $scope.global.userData = $firebase($scope.global.userRef);
+      $scope.global.tasksData = $firebase($scope.global.tasksRef);
+    }
+    else {
+      console.log(user);
+      $scope.global.user = user;
+      //console.log('user not logged in.');
+    }
+
+  })
+
+  $scope.globalFn.signIn = function()
+  {
+    $scope.global.auth.login('password', {
+      email: $scope.global.authuser.email,
+      password: $scope.global.authuser.password
+    });
+  }
+
+  $scope.globalFn.signOut = function()
+  {
+    $scope.global.auth.logout();
+  }
+
+  $scope.globalFn.signUp = function()
+  {
+    $scope.global.auth.createUser($scope.global.authuser.email, $scope.global.authuser.password, function(error, user)
+    {
+      if( error )
+      {
+        console.log(error);
+      }
+      else
+      {
+        console.log(user);
+      }
+    })
+  }
+
+
+  $scope.globalFn.addTask = function()
+  {
+
+    if($scope.global.taskToAdd && $scope.global.user.id)
+    {
+      $scope.global.tasksData.$add($scope.global.taskToAdd);
+      console.log('task added');
+    }
+    //$scope.m.taskToAdd = null;
+  }
+
+
+
+}])
+
